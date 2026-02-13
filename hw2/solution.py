409b51625688d082fb79ed413051b1dab37719b7
Korimer <thekclov@gmail.com>
2026-02-13 15:51:29 -0700

    from scratch lol

--- hw2/solution.py
+++ hw2/solution.py
@@ -1,48 +1,10 @@
-# pyright: basic
-from __future__ import annotations
-from typing import Optional, override
-
-class NodeData:
-    def __init__(self, val=None) -> None:
-        self._val = val
-        self._l = TreeNode()
-        self._r = TreeNode()
-
-    def insert(self, val) -> None:
-        if val == self._val: raise Exception("Cannot have duplicate values!")
-        return self._get(val).insert(val)
-
-    def _get(self,val) -> TreeNode:
-        if val < self._val: return self._l
-        else:               return self._r
-
-    @override
-    def __str__(self) -> str:
-        return f"({self._val} -> {self._l}, {self._r})"
-
-
-class TreeNode:
-    def __init__(self, node: Optional[NodeData] = None) -> None:
-        self._inr = node
-
-    def insert(self,val):
-        if self._inr == None:
-            self._inr = NodeData(val)
-            self._empty = False
-        else:
-            self._inr.insert(val)
-
-    @override
-    def __str__(self) -> str:
-        return f"[{self._inr}]"
-    @override
-    def __repr__(self) -> str:
-        return str(self._inr)
-
-
-t = TreeNode()
-t.insert("hi!")
-t.insert("bye...")
-
-if __name__ == "main":
+if __name__ == "__main__":
     print("lol")
+
+class AsIfATree:
+    def __init__(self) -> None:
+        self.core = []
+        self.len = 0
+
+    def height(self) -> int:
+        return 0
