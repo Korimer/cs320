d9e26b8c76de8925d28bfea4a711756b7ed8f7ba
Korimer <thekclov@gmail.com>
2026-02-16 23:50:51 -0700

    ok we actually have something now lol

--- hw2/solution.py
+++ hw2/solution.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+from enum import Enum
+
+
 if __name__ == "__main__":
     print("lol")
 
@@ -8,3 +12,51 @@ class AsIfATree:
 
     def height(self) -> int:
         return 0
+
+    def width(self) -> int:
+        return 1
+
+    def childOf(self, nodenum: int, direction: Direction) -> int:
+        return (nodenum*2) + (direction.value)
+
+    def find(self,val,populate_on_oob=False):
+
+        def findhelper(pos: int,val) -> int:
+            if populate_on_oob:
+                self.fillTo(pos)
+            elif pos < len(self): 
+                return -1
+
+            if self.core[pos] == None:
+                return pos
+            else:
+                direction = Direction.Left if self.core[pos] < val else Direction.Right
+                nextpos = self.childOf(pos,direction)
+                return findhelper(nextpos,val)
+
+        return findhelper(0,val)
+
+    def insert(self,val):
+        self.core[self.find(val,populate_on_oob=True)] = val
+
+    def fillTo(self,minsize: int):
+        toappend = max(0,minsize-len(self)+1)
+        self.len += toappend
+        self.core += [ None ] * toappend
+
+    def __len__(self) -> int:
+        return self.len
+            
+
+class Direction(Enum):
+    Left = 1
+    Right = 2
+
+
+tree = AsIfATree()
+
+tree.insert(1)
+tree.insert(2)
+tree.insert(5)
+tree.insert(-1)
+print(tree.core)
