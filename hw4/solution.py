96d75296e5f7f78a5d48afe2cabc80b81335abdf
Korimer <thekclov@gmail.com>
2026-03-11 15:32:38 -0600

    slightly pep8

--- hw4/solution.py
+++ hw4/solution.py
@@ -4,30 +4,39 @@
 from math import ceil, log
 
 
-def getRadixes(list: list[int]) -> tuple[list[int],...]:
+class HistoricNum:
+    orig_num: int
+    cur_num: int
+    def __init__(self, orig_num, cur_num):
+        self.orig_num = orig_num
+        self.cur_num = cur_num
 
-    prefixes = tuple([[] for _ in range(9)])
+
+def getRadixes(list: list[HistoricNum], base) -> tuple[list[HistoricNum], ...]:
+
+    prefixes = tuple([[] for _ in range(base-1)])
 
     for num in list:
-        quotient, divisor = divmod(num,10)
-        prefixes[quotient].append(divisor)
+        quotient, divisor = divmod(num.cur_num, base)
+        num.orig_num = divisor
+        prefixes[quotient].append(num)
 
     return prefixes
 
 
 def getMaxIterations(all_nums: list[int], base: int) -> int:
-    return ceil(log(max(all_nums),base)) -1
+    return ceil(log(max(all_nums), base)) -1
 
 
-def radixHelper(numlist, iter_count):
+def radixHelper(numlist, base, iter_count):
     if iter_count == 0: return numlist
     return [
-        radixHelper(digit,iter_count-1)
-        for digit in getRadixes(numlist)
+        radixHelper(digit, base, iter_count-1)
+        for digit in getRadixes(numlist, base)
     ]
 
 
-def unfoldNTimes(list,times):
+def unfoldNTimes(list, times):
     unfolded = list
     for _ in range(times):
         unfolded = [
@@ -38,9 +47,9 @@ def unfoldNTimes(list,times):
     return unfolded
 
 
-def radix_base(values_to_sort,base):
-    max_depth = getMaxIterations(values_to_sort,base)
-    sorted = radixHelper(values_to_sort,max_depth)
-    unfolded = unfoldNTimes(sorted,max_depth)
+def radix_base(values_to_sort, base):
+    max_depth = getMaxIterations(values_to_sort, base)
+    sorted = radixHelper(values_to_sort, base, max_depth)
+    unfolded = unfoldNTimes(sorted, max_depth)
     sort_desc = unfolded[::-1]
     return sort_desc
