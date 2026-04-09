744df352ef26675760ae58f9ff078439439a938d
Korimer <thekclov@gmail.com>
2026-04-08 22:24:38 -0600

    wow that sucked

--- hw7/solution.py
+++ hw7/solution.py
@@ -1,34 +1,42 @@
 # pyright: basic
 
-from queue import Queue
+from collections import deque
 import edgegraph
 
 
 def bfs(graph: edgegraph.GraphEL, start: edgegraph.VertexEL) -> list:
+
+    if graph is None or start is None:
+        raise ValueError("Invalid graph or vertex")
+
+    if start.name not in graph._vertices:
+        return []
+
     visited = set()
-    tovisit = Queue()
+    tovisit = deque()
     searchresults = []
 
-    tovisit.put(start)
+    tovisit.append(start)
 
-    while not tovisit.empty():
+    while len(tovisit) != 0:
+        searchresults.append(tuple(tovisit))
+
         connections = set()
-        ordered_connections = Queue()
-        while not tovisit.empty():
-            nextnode = tovisit.get()
+        ordered_connections = deque()
+        while len(tovisit) != 0:
+            nextnode = tovisit.popleft()
             if nextnode in visited:
                 continue
+
             visited.add(nextnode)
-            searchresults.append(nextnode)
 
             all_connections = graph.adjacent(nextnode)
 
             for connectednode in all_connections:
                 if connectednode not in connections and connectednode not in visited:
                     connections.add(connectednode)
-                    ordered_connections.put(connectednode)
-        
+                    ordered_connections.append(connectednode)
+            
         tovisit = ordered_connections
-
                     
     return searchresults
