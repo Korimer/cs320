4220a6057e93a6ea15c987cd6b4bfd7506d2e74d
Korimer <thekclov@gmail.com>
2026-04-08 21:24:52 -0600

    first try?

--- hw7/solution.py
+++ hw7/solution.py
@@ -1,14 +1,33 @@
 # pyright: basic
 
+from queue import Queue
 import edgegraph
+
 
 def bfs(graph: edgegraph.GraphEL, start: edgegraph.VertexEL) -> list:
     visited = set()
-    tovisit = {start}
-    next = set()
+    tovisit = Queue()
+    searchresults = []
 
-    while len(tovisit) != 0:
-        for nodename in tovisit:
-            pass
-        tovisit = next
+    tovisit.put(start)
+
+    while not tovisit.empty():
+        connections = set()
+        ordered_connections = Queue()
+        while not tovisit.empty():
+            nextnode = tovisit.get()
+            if nextnode in visited:
+                continue
+            visited.add(nextnode)
+            searchresults.append(nextnode)
+
+            all_connections = graph.adjacent(nextnode)
+
+            for connectednode in all_connections:
+                if connectednode not in connections and connectednode not in visited:
+                    connections.add(connectednode)
+                    ordered_connections.put(connectednode)
+
+            tovisit = ordered_connections
+                    
     return []
