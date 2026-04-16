e0099578e92bbe0fc526a65853b211deddb52233
Korimer <thekclov@gmail.com>
2026-04-15 23:06:35 -0600

    progress

--- hw8/solution.py
+++ hw8/solution.py
@@ -12,28 +12,39 @@ class PalindromeTraversal:
     def __init__(self, graph: GraphEL) -> None:
         self.graph: GraphEL = graph
         self.verticies: list[VertexEL] = graph.vertices()
-        vertexvalues = [edge.tail().get_value() for edge in graph.edges()]
+        vertexvalues = [edge.head().get_value() for edge in graph.edges()]
         self.vremaining: Counter = Counter(vertexvalues)
         self.visited: set[EdgeEL] = set()
         self.curroute: list[EdgeEL] = []
-        self.palindromes: list[list] = []
+        self.palindromes: list[tuple[int, ...]] = []
 
+    def getAllPalindromes(self) -> list[tuple[int, ...]]:
+        for vert in self.verticies:
+            self._visit(vert)
+        return self.palindromes
 
     def _visit(self, vert: VertexEL) -> None:
         self.vremaining.subtract([vert])
 
         if self.isPalindrome():
-            self.palindromes.append(self.curroute)
+            edgevals = [edge.get_value() for edge in self.curroute]
+            self.palindromes.append(tuple(edgevals))
 
         unvisited_connections = [
-            v_adjacent for v_adjacent in self.graph.outgoing(vert)
+            v_adjacent for v_adjacent in self.graph.incident(vert)
             if v_adjacent not in self.visited
         ]
 
-        valid_followups = self.canCreatePalindromeWith()
+        # valid_followups = self.canCreatePalindromeWith()
         for adjacent in unvisited_connections:
-            if valid_followups is None or adjacent.get_value() in valid_followups:
-                self._visit(adjacent)
+            adj1, adj2 = adjacent.ends()
+            nextvert = adj1 if vert == adj2 else adj2
+            # if valid_followups is None or adjacent.get_value() in valid_followups:
+            self.visited.add(adjacent)
+            self.curroute.append(adjacent)
+            self._visit(nextvert)
+            self.curroute.pop()
+            self.visited.remove(adjacent)
 
         self.vremaining.update([vert])
 
@@ -41,18 +52,14 @@ class PalindromeTraversal:
         if len(self.curroute) < 3:
             return False
 
-        for i in range(len(self.curroute) // 2):
-            if self.curroute[i] != self.curroute[i * -1]:
-                return False
+        edgevals = [edge.get_value() for edge in self.curroute]
 
-        return True
+        return edgevals == edgevals[::-1]
 
     def canCreatePalindromeWith(self) -> Optional[set]:
         return set()
 
-
-
-
-def pld_graph(graph: GraphEL) -> list[tuple[int,...]]:
-    return []
-
+def pld_graph(graph: Optional[GraphEL]) -> list[tuple]:
+    if graph is None:
+        raise ValueError("Bad graph")
+    return PalindromeTraversal(graph).getAllPalindromes()
